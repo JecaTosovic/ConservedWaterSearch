6191b12aaa71e98a7116aac3369f8282adbd8501
Domagoj Fijan <50439291+DomFijan@users.noreply.github.com>
2023-02-12 00:12:43 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2022, J Tosovic"
 author = "Jelena Tosovic, Domagoj Fijan, Marko Jukic, Urban Bren"
 
 # The full version, including alpha/beta/rc tags
-version = "0.1.0"
+version = "0.1.1"
 release = "0.1.1"
 
 
